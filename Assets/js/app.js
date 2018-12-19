8a3cec7dd540d1d2afac49b512d7800abd694a8e
Tyler Tompa <tylertompa@email.com>
2018-12-19 01:05:48 -0500

    set up svg canvas

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,3 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Tyler Tompa
+2018, December, 19
+UNCC Data Analytics Boot Camp, Homework, Unit 16- D3
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Step 1: Set up our chart
+var svg_width = 960;
+var svg_height = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+
+var width = svg_width - margin.left - margin.right;
+var height = svg_height - margin.top - margin.bottom;
+
+/*
+Step 2:
+Create an SVF wrapper,
+append an SVG group that will hold our chart,
+shift the latter by left and top margins
+*/
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svg_width)
+  .attr("height", svg_height);
+
 d3.csv("../../Data/data.csv", function() {
     console.log("Found the csv file.");
 
